6e71a12d65f3902e56b19f40ddfa4fd672c14177
Matt Micene (RHT) <mmicene@redhat.com>
2025-11-06 16:53:35 -0500

    New infra configs

--- setup-automation/setup-builder.sh
+++ setup-automation/setup-builder.sh
@@ -12,7 +12,11 @@ cat<<EOF> ~/.config/containers/auth.json
     "auths": {
       "registry.redhat.io": {
         "auth": "${REGISTRY_PULL_TOKEN}"
-      }
+      },
+      "registry.stage.redhat.io": {
+			"auth":
+			"${STAGE_KEY}"
+	}
     }
   }
 EOF
@@ -27,6 +31,11 @@ podman pull registry.redhat.io/rhel9/rhel-bootc:$BOOTC_RHEL_VER registry.redhat.
 # RHEL 10 bases
 BOOTC_RHEL_VER=10.0
 podman pull registry.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER registry.redhat.io/rhel10/bootc-image-builder:$BOOTC_RHEL_VER
+BOOTC_RHEL_VER=10.1
+podman pull registry.stage.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER registry.stage.redhat.io/rhel10/bootc-image-builder:$BOOTC_RHEL_VER
+
+# Remove pull credentials
+# rm ~/.config/containers/auth.json
 
 # set up SSL for fully functioning registry
 # Enable EPEL for RHEL 10
@@ -59,89 +68,6 @@ podman run --privileged -d \
 mkdir -p ~/etc/sudoers.d/
 echo "%wheel  ALL=(ALL)   NOPASSWD: ALL" >> ~/etc/sudoers.d/wheel
 
-# turn off complexity checks in target for passwords to ease the learner experience
-mkdir -p ~/etc/security
-cat <<EOF> ~/etc/security/pwquality.conf
-# Configuration for systemwide password quality limits
-# Defaults:
-#
-# Number of characters in the new password that must not be present in the
-# old password.
-# difok = 1
-#
-# Minimum acceptable size for the new password (plus one if
-# credits are not disabled which is the default). (See pam_cracklib manual.)
-# Cannot be set to lower value than 6.
-minlen = 6
-#
-# The maximum credit for having digits in the new password. If less than 0
-# it is the minimum number of digits in the new password.
-# dcredit = 0
-#
-# The maximum credit for having uppercase characters in the new password.
-# If less than 0 it is the minimum number of uppercase characters in the new
-# password.
-# ucredit = 0
-#
-# The maximum credit for having lowercase characters in the new password.
-# If less than 0 it is the minimum number of lowercase characters in the new
-# password.
-# lcredit = 0
-#
-# The maximum credit for having other characters in the new password.
-# If less than 0 it is the minimum number of other characters in the new
-# password.
-# ocredit = 0
-#
-# The minimum number of required classes of characters for the new
-# password (digits, uppercase, lowercase, others).
-# minclass = 0
-#
-# The maximum number of allowed consecutive same characters in the new password.
-# The check is disabled if the value is 0.
-# maxrepeat = 0
-#
-# The maximum number of allowed consecutive characters of the same class in the
-# new password.
-# The check is disabled if the value is 0.
-# maxclassrepeat = 0
-#
-# Whether to check for the words from the passwd entry GECOS string of the user.
-# The check is enabled if the value is not 0.
-# gecoscheck = 0
-#
-# Whether to check for the words from the cracklib dictionary.
-# The check is enabled if the value is not 0.
-dictcheck = 0
-#
-# Whether to check if it contains the user name in some form.
-# The check is enabled if the value is not 0.
-# usercheck = 0
-#
-# Length of substrings from the username to check for in the password
-# The check is enabled if the value is greater than 0 and usercheck is enabled.
-# usersubstr = 0
-#
-# Whether the check is enforced by the PAM module and possibly other
-# applications.
-# The new password is rejected if it fails the check and the value is not 0.
-enforcing = 0
-#
-# Path to the cracklib dictionaries. Default is to use the cracklib default.
-# dictpath =
-#
-# Prompt user at most N times before returning with error. The default is 1.
-# retry = 3
-#
-# Enforces pwquality checks on the root user password.
-# Enabled if the option is present.
-# enforce_for_root
-#
-# Skip testing the password quality for users that are not present in the
-# /etc/passwd file.
-# Enabled if the opt
-EOF
-
 # create config.json for BIB to add a user / pass
 cat <<EOF> ~/config.json
 {
@@ -159,16 +85,6 @@ cat <<EOF> ~/config.json
     }
   }
 }
-EOF
-
-# create basic UBI containerfile
-cat <<EOF> /root/Containerfile
-FROM registry.access.redhat.com/ubi9/ubi
-
-RUN dnf install -y httpd
-RUN echo "Hello Red Hat" > /var/www/html/index.html
-
-ENTRYPOINT /usr/sbin/httpd -DFOREGROUND
 EOF
 
 # create basic bootc containerfile
