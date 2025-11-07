3004d4898c8afd15174a2dae6733010476fbb78d
Matt Micene (RHT) <mmicene@redhat.com>
2025-11-07 10:41:50 -0500

    add samples folder, git clone, setup organiztion

--- setup-automation/setup-builder.sh
+++ setup-automation/setup-builder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
+# Set up the infrastructure environemnt
 # Packages are in instances.yaml, turn on libvirtd and set up nss support
 systemctl enable --now libvirtd
 sed -i 's/hosts:\s\+ files/& libvirt libvirt_guest/' /etc/nsswitch.conf
@@ -23,14 +24,12 @@ EOF
 
 # Pull the needed images to minimize waiting during the lab
 # Will also need staging and creds for testing
-# UBI
-podman pull registry.access.redhat.com/ubi9/ubi
 # RHEL 9.6 bases
-BOOTC_RHEL_VER=9.6
-podman pull registry.redhat.io/rhel9/rhel-bootc:$BOOTC_RHEL_VER registry.redhat.io/rhel9/bootc-image-builder:$BOOTC_RHEL_VER
+# BOOTC_RHEL_VER=9.6
+# podman pull registry.redhat.io/rhel9/rhel-bootc:$BOOTC_RHEL_VER registry.redhat.io/rhel9/bootc-image-builder:$BOOTC_RHEL_VER
 # RHEL 10 bases
-BOOTC_RHEL_VER=10.0
-podman pull registry.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER registry.redhat.io/rhel10/bootc-image-builder:$BOOTC_RHEL_VER
+# BOOTC_RHEL_VER=10.0
+# podman pull registry.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER registry.redhat.io/rhel10/bootc-image-builder:$BOOTC_RHEL_VER
 BOOTC_RHEL_VER=10.1
 podman pull registry.stage.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER registry.stage.redhat.io/rhel10/bootc-image-builder:$BOOTC_RHEL_VER
 
@@ -63,41 +62,6 @@ podman run --privileged -d \
   -e REGISTRY_HTTP_TLS_KEY=/certs/privkey.pem \
   quay.io/mmicene/registry:2
 
-# For the target bootc system build, we need to set up a few config files to operate in the lab environment
-# create sudoers drop in and etc structure to add to container
-mkdir -p ~/etc/sudoers.d/
-echo "%wheel  ALL=(ALL)   NOPASSWD: ALL" >> ~/etc/sudoers.d/wheel
-
-# create config.json for BIB to add a user / pass
-cat <<EOF> ~/config.json
-{
-  "blueprint": {
-    "customizations": {
-      "user": [
-        {
-          "name": "core",
-          "password": "redhat",
-           "groups": [
-	            "wheel"
-	          ]
-        }
-      ]
-    }
-  }
-}
-EOF
-
-# create basic bootc containerfile
-cat <<EOF> /root/Containerfile.el10
-FROM registry.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER
-
-ADD etc /etc
-
-RUN dnf install -y httpd
-RUN systemctl enable httpd
-
-EOF
-
 # Add name based resolution for internal IPs
 echo "10.0.2.2 builder.${GUID}.${DOMAIN}" >> /etc/hosts
 echo "10.0.2.2 registry-${GUID}.${DOMAIN}" >> /etc/hosts
@@ -105,7 +69,7 @@ cp /etc/hosts ~/etc/hosts
 
 # Script that manages the VM SSH session tab
 # Waits for the domain to start and networking before attempting to SSH to guest
-cat <<'EOF'> /root/wait_for_bootc_vm.sh
+cat <<'EOF'> /root/.wait_for_bootc_vm.sh
 echo "Waiting for VM 'bootc-vm' to be running..."
 VM_READY=false
 VM_STATE=""
@@ -130,11 +94,11 @@ done
 ssh core@${VM_NAME}
 EOF
 
-chmod u+x /root/wait_for_bootc_vm.sh
+chmod u+x /root/.wait_for_bootc_vm.sh
 #
 # Script that manages the ISO SSH session tab
 # Waits for the domain to start and networking before attempting to SSH to guest
-cat <<'EOF'> /root/wait_for_iso_vm.sh
+cat <<'EOF'> /root/.wait_for_iso_vm.sh
 echo "Waiting for VM 'iso-vm' to be running..."
 VM_READY=false
 VM_STATE=""
@@ -159,4 +123,44 @@ done
 ssh core@${VM_NAME}
 EOF
 
-chmod u+x /root/wait_for_iso_vm.sh
+chmod u+x /root/.wait_for_iso_vm.sh
+
+# Set up the exercise specifics
+#
+# create internet example bootc containerfile
+cat <<EOF> /root/Containerfile.el10
+FROM registry.redhat.io/rhel10/rhel-bootc:$BOOTC_RHEL_VER
+
+RUN echo "%wheel  ALL=(ALL)   NOPASSWD: ALL" >> /etc/sudoers.d/wheel
+
+RUN dnf install -y httpd
+RUN systemctl enable httpd
+
+EOF
+
+# Clone the git repo for the application to deploy
+git clone --single-branch --branch bootc https://github.com/nzwulfin/python-pol.git bootc-version
+
+# Copy the samples folder to the lab user home directory
+cp /tmp/samples ~/samples
+
+# create config.json for BIB to add a user / pass
+#cat <<EOF> ~/config.json
+#{
+#  "blueprint": {
+#    "customizations": {
+#      "user": [
+#        {
+#          "name": "core",
+#          "password": "redhat",
+#           "groups": [
+#	            "wheel"
+#	          ]
+#        }
+#      ]
+#    }
+#  }
+#}
+#EOF
+
+
